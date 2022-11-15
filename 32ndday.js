be26e188e9f1f469b1a0ed6a58dbbeccfa4d6900
kairatbek-d <kairatbekdavranbekov@gmail.com>
2022-11-15 14:57:09 +0600

    32 finish

--- 32ndday.js
+++ 32ndday.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var titleToNumber = function(columnTitle) {
-    let result = 0;
-    for(let c in columnTitle) {
-		//d = s[i](char) - 'A' + 1 (we used  s[i] -  'A' to convert the letter to a number like it's going to be C)
-
-        let d = c.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
-        console.log(d)
-        result = result * 26 + d;
+    let total = 0;
+    for (let i = 0; i < columnTitle.length; i +=1 ) {
+        total = total * 26 + columnTitle.charCodeAt(i) - "A".charCodeAt(0) + 1;
     }
-    return result;
+    return total;
 };
 
-console.log(titleToNumber("ZY"));
+console.log(titleToNumber("AZ"));
